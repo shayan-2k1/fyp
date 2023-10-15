9d3ca8c7df587b680dde6e5788fee1f6e7af823e
Muhammad Shayan Khan <shayan.956khan@gmail.com>
2023-10-15 21:01:57 +0500

    Specified routing and set up sign in screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Desktop1, { HomeStudent } from '../components/HomeStudent'
+import './App.scss';
+import {Routes,Route} from 'react-router-dom'
+import Navigation from './routes/navigation/navigation.component.jsx'
+import Home from './routes/home/home.component.jsx'
+import SignIn from './routes/sign-in/sign-in.component';
 
 function App() {
   return (
-    <>
-    
-    </>
-  )
+    <Routes>
+      <Route path='/' element={<Navigation/>}> 
+        <Route index element={<Home/>}/>
+        <Route path='sign-in' element={<SignIn/>}/>
+        <Route path='wishlist'/>
+        <Route path='chat'/>
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/routes/home/home.component.jsx
@@ -0,0 +1,11 @@
+const Home =()=>{
+    return(
+        <div>
+            <div className="hero-section">
+                
+            </div>
+        </div>
+    );
+};
+
+export default Home;
