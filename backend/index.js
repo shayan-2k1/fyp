768f75f31b7741cb4964478b5bac96385d6ce11c
12MaryamZafar <12maryamzafar@gmail.com>
2023-10-16 22:22:37 +0500

    registration

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express= require("express"); 
+//imports the Express library, which provides a framework for building web applications in Node.js
+const mongoose=require("mongoose")
+const studentRoute = require("./Routes/RegistrationRoutes.js")
+
+const cors = require('cors');
+
+require("dotenv").config();
+const app= express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+app.use("/student" , studentRoute);
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`App listening on port ${process.env.PORT}`)
+})
+
+mongoose.connect(process.env.MONGO_URL).then( ()=>{ 
+     console.log("connected")
+}).catch(err=>{
+    console.log(err)
+})
